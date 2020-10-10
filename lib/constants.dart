88b6a840f91d03bd33ed627bad717cf6205867a1
avinashkranjan <ranjan.avinash@hotmail.com>
2020-10-10 15:10:23 +0530

    checkpoint commit

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const kCardColor = Color(0xFF282B30);
+const kHourColor = Color(0xFFF5C35A);
+const kBGColor = Color(0xFF343537);
+
+const kTextColor = Color(0xFF6C7174);
+
+const kCalendarDay = TextStyle(
+  color: kTextColor,
+  fontSize: 16.0,
+);
--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Class Manager',
       theme: ThemeData(
         // This is the theme of your application.
         //
