b16c116d8c71ec76716fe6e21df7f537961a7124
Adarsh Srivastava <imadarshsri@gmail.com>
2021-01-09 00:35:49 +0530

    Adds Authentication Services
    This adds Firebase Authentication Services for Login/Sigup Process.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:class_manager/screens/welcome_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 
