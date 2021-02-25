56b6831ffae29f1dca5e61099404ce1e18db620a
Ajay <ajay963@outlook.com>
2021-02-25 23:45:47 +0530

    calender with tap function

--- lib/screens/classes_screen.dart
+++ lib/screens/classes_screen.dart
@@ -11,6 +11,26 @@ class ClassesScreen extends StatefulWidget {
 
 class _ClassesScreenState extends State<ClassesScreen> {
   final DateTime _today = DateTime.now();
+  int _calenderItemIndex = 0;
+  _setCalenderIndex({@required int index}) {
+    setState(() {
+      _calenderItemIndex = index;
+      print(_calenderItemIndex);
+    });
+  }
+
+  @override
+  void initState() {
+    _calenderItemIndex = 0;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _calenderItemIndex = 0;
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -33,38 +53,60 @@ class _ClassesScreenState extends State<ClassesScreen> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Text(_today.subtract(Duration(days: 3)).day.toString(),
-                      style: kCalendarDay),
-                  Text(_today.subtract(Duration(days: 2)).day.toString(),
-                      style: kCalendarDay),
-                  Text(_today.subtract(Duration(days: 1)).day.toString(),
-                      style: kCalendarDay),
-                  Text(
-                    _today.day.toString(),
-                    style: kCalendarDay.copyWith(
-                      color: Colors.white,
-                      fontSize: 17.0,
-                      fontWeight: FontWeight.w500,
+                  InkWell(
+                    onTap: () => _setCalenderIndex(index: -3),
+                    child: CalenderDateFormatAddition(
+                      dateTime: _today,
+                      dayAdditon: -3,
+                      itemSelected: (_calenderItemIndex == -3) ? true : false,
                     ),
                   ),
-                  Text(_today.add(Duration(days: 1)).day.toString(),
-                      style: kCalendarDay),
-                  Text(_today.add(Duration(days: 2)).day.toString(),
-                      style: kCalendarDay),
-                  Text(_today.add(Duration(days: 3)).day.toString(),
-                      style: kCalendarDay),
-                ],
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 157.0, top: 3.0),
-                child: Text(
-                  DateFormat.E().format(_today).toString(),
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 17.0,
-                    fontWeight: FontWeight.w500,
+                  InkWell(
+                    onTap: () => _setCalenderIndex(index: -2),
+                    child: CalenderDateFormatAddition(
+                      dateTime: _today,
+                      dayAdditon: -2,
+                      itemSelected: (_calenderItemIndex == -2) ? true : false,
+                    ),
                   ),
-                ),
+                  InkWell(
+                    onTap: () => _setCalenderIndex(index: -1),
+                    child: CalenderDateFormatAddition(
+                      dateTime: _today,
+                      dayAdditon: -1,
+                      itemSelected: (_calenderItemIndex == -1) ? true : false,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () => _setCalenderIndex(index: 0),
+                    child: CalenderDateFormatAddition(
+                      dateTime: _today,
+                      dayAdditon: 0,
+                      itemSelected: (_calenderItemIndex == 0) ? true : false,
+                    ),
+                  ),
+                  InkWell(
+                      onTap: () => _setCalenderIndex(index: 1),
+                      child: CalenderDateFormatAddition(
+                        dateTime: _today,
+                        dayAdditon: 1,
+                        itemSelected: (_calenderItemIndex == 1) ? true : false,
+                      )),
+                  InkWell(
+                      onTap: () => _setCalenderIndex(index: 2),
+                      child: CalenderDateFormatAddition(
+                        dateTime: _today,
+                        dayAdditon: 2,
+                        itemSelected: (_calenderItemIndex == 2) ? true : false,
+                      )),
+                  InkWell(
+                      onTap: () => _setCalenderIndex(index: 3),
+                      child: CalenderDateFormatAddition(
+                        dateTime: _today,
+                        dayAdditon: 3,
+                        itemSelected: (_calenderItemIndex == 3) ? true : false,
+                      )),
+                ],
               ),
             ],
           ),
@@ -85,6 +127,60 @@ class _ClassesScreenState extends State<ClassesScreen> {
           ),
         ),
       ],
+    );
+  }
+}
+
+class CalenderDateFormatAddition extends StatelessWidget {
+  final DateTime dateTime;
+  final int dayAdditon;
+  final bool itemSelected;
+  CalenderDateFormatAddition(
+      {@required this.dateTime,
+      @required this.dayAdditon,
+      this.itemSelected = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 400),
+      child: RichText(
+        text: TextSpan(children: [
+          TextSpan(
+              text: (dayAdditon > 0)
+                  ? dateTime.add(Duration(days: dayAdditon)).day.toString() +
+                      '\n'
+                  : dateTime
+                          .subtract(Duration(days: dayAdditon * -1))
+                          .day
+                          .toString() +
+                      '\n',
+              style: (itemSelected == true)
+                  ? kCalendarDay.copyWith(
+                      color: Colors.white,
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.w500,
+                    )
+                  : kCalendarDay),
+          TextSpan(
+            text: (itemSelected == true)
+                ? (dayAdditon > 0)
+                    ? DateFormat.E()
+                        .format(dateTime.add(Duration(days: dayAdditon)))
+                        .toString()
+                    : DateFormat.E()
+                        .format(
+                            dateTime.subtract(Duration(days: dayAdditon * -1)))
+                        .toString()
+                : '',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 17.0,
+              fontWeight: FontWeight.w500,
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
