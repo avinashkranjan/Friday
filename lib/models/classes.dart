c287ab4cbd1a9f03b4d3c4e79bfb172b81a92eea
Nikki Goel <gupta.nikkigupta@gmail.com>
2021-03-15 14:26:26 +0530

    Classes data source changed
    dummy classes data was added in firestore and is being displayed instead of hardcoded data

--- lib/models/classes.dart
+++ lib/models/classes.dart
@@ -28,6 +28,24 @@ class Classes {
       };
 }
 
+Future<List<Classes>> getClassList() async {
+  List<Classes> classesList = [];
+  return FirebaseFirestore.instance
+      .collection('colleges')
+      .doc('USICT-BTECH-CSE-3')
+      .get()
+      .then((documentSnapshot) {
+    if (documentSnapshot.exists) {
+      final classesMap = documentSnapshot.data()['classes'] as Map;
+
+      classesMap.forEach((key, value) {
+        classesList.add(Classes.fromMap(value));
+      });
+    }
+    return classesList;
+  });
+}
+
 List<Classes> classes = [
   Classes(
     subject: "Math",
@@ -58,70 +76,24 @@ List<Classes> classes = [
 Map<String, Classes> generateDummyClasses() {
   final uuid = Uuid();
 
-  Map<String, Classes> data = {
-    uuid.v4(): Classes(
-      subject: "Math",
-      type: "Online Class",
-      teacherName: "Julie Raybon",
-      time: DateTime.parse("2020-06-04 10:30:00"),
-    ),
-    uuid.v4(): Classes(
-      subject: "German",
-      type: "Online Class",
-      teacherName: "Mary Peterson",
-      time: DateTime.parse("2020-06-06 06:30:00"),
-    ),
-    uuid.v4(): Classes(
-      subject: "German",
-      type: "Online Class",
-      teacherName: "Mary Peterson",
-      time: DateTime.parse("2020-06-06 06:30:00"),
-    ),
-    uuid.v4(): Classes(
-      subject: "History",
-      type: "Online Class",
-      teacherName: "Jim Brooke",
-      time: DateTime.parse("2020-06-06 07:30:00"),
-    ),
-  };
+  Map<String, Classes> data = {};
+
+  classes.forEach((element) {
+    data[uuid.v4()] = element;
+  });
 
   return data;
 }
 
-Future<void> addClassesToFirestore() async {
+Future<void> addDummyClassesToFirestore() async {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
   final DocumentReference documentReference =
       _firestore.collection('colleges').doc('USICT-BTECH-CSE-3');
-
   final Map<String, Classes> classesData = generateDummyClasses();
-
-  // final newMap = classesData.map((key, value) {
-  //   return MapEntry(key, value.toMap());
-  // });
-
-  // final classesMap = {'classes': newMap};
-
-  // print(classesMap);
 
   documentReference.update({
     'classes': classesData.map((key, value) {
       return MapEntry(key, value.toMap());
     })
-  });
-}
-
-Future<List<Classes>> getClassList() async {
-  // addClassesToFirestore();
-  List classesMap = [];
-  return FirebaseFirestore.instance
-      .collection('colleges')
-      .doc('USICT-BTECH-CSE-3')
-      .get()
-      .then((documentSnapshot) {
-    classesMap = documentSnapshot.data()['classes'] as List;
-    return classesMap.map((e) {
-      return Classes.fromMap(e);
-    }).toList();
   });
 }
