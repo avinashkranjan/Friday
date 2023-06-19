51680ae82da6fa0778d604d1ab861ef8281bc8b8
andoriyaprashant <prashantandoriya@gmail.com>
2023-06-19 23:29:45 +0530

    lib/services/user_info_services.dart

--- lib/services/user_info_services.dart
+++ lib/services/user_info_services.dart
@@ -2,12 +2,54 @@ import 'package:friday/models/users.dart';
 import 'package:friday/services/user_db_services.dart';
 import 'package:flutter/material.dart';
 
+class User {
+  late String uid;
+  late String profilePictureUrl;
+  late String name;
+  late String email;
+  late String course;
+  late String dept;
+  late String college;
+  late int year;
+  late Gender gender;
+  late int age;
+
+  User({
+    required this.uid,
+    required this.profilePictureUrl,
+    required this.name,
+    required this.email,
+    required this.course,
+    required this.dept,
+    required this.college,
+    required this.year,
+    required this.gender,
+    required this.age,
+  });
+
+  User setEssentialDetails(String name, String email) {
+    return User(
+      uid: this.uid,
+      profilePictureUrl: this.profilePictureUrl,
+      name: name,
+      email: email,
+      course: this.course,
+      dept: this.dept,
+      college: this.college,
+      year: this.year,
+      gender: this.gender,
+      age: this.age,
+    );
+  }
+
+  void setAdditionalDetails(String course, String dept, String college, int year, Gender gender, int age) {}
+}
 class UserInfoServices extends ChangeNotifier {
-  Users _user;
+  late User _user;
   bool hasData = false;
 
-  Users get user => _user;
-  Future<bool> fetchUserDetailsFromDatabase(context) async {
+  User get user => _user;
+  Future<bool> fetchUserDetailsFromDatabase(BuildContext context) async {
     // Write the code to fetch from Firestore Collection `users`
     bool isUserExists = await UserDBServices.fetchUserData(context);
     if (!isUserExists) {
@@ -24,26 +66,32 @@ class UserInfoServices extends ChangeNotifier {
 
   Future<void> upateProfilePictureUrl() async {
     await UserDBServices.updateProfilePictureUrl(
-        _user.uid, _user.profilePictureUrl);
+        _user.uid,
+        _user.profilePictureUrl
+    );
     // notifyListeners();
   }
 
-  void setUser(Users _usr) {
-    this._user = _usr;
+  void setUser(User user) {
+    this._user = user;
     this.hasData = true;
     notifyListeners();
   }
 
   void setEssentialDetailsOfUser(String name, String email) {
-    this._user = Users.setEsssentialDetails(name, email);
+    this._user = _user.setEssentialDetails(name, email);
   }
 
-  void setAdditionalDetailsOfUser(String _course, String _dept, String _college,
-      int _year, Gender _gender, int _age,
-      [String _profilePicUrl]) {
-    this
-        ._user
-        .setAdditionalDetails(_course, _dept, _college, _year, _gender, _age);
+  void setAdditionalDetailsOfUser(
+    String _course, 
+    String _dept, 
+    String _college,
+    int _year, 
+    Gender _gender, 
+    int _age,
+    [String? _profilePicUrl]) {
+
+    this._user.setAdditionalDetails(_course, _dept, _college, _year, _gender, _age);
     notifyListeners();
   }
 }
