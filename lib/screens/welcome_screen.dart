8c42d002d6ce641b7dcf029bf054be4af5b8dc17
andoriyaprashant <prashantandoriya@gmail.com>
2023-06-28 00:16:17 +0530

    modified:

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -28,7 +28,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
               height: MediaQuery.of(context).size.height - 100,
               width: MediaQuery.of(context).size.width,
               decoration: BoxDecoration(
-                color: Theme.of(context).backgroundColor,
+                color: Theme.of(context).colorScheme.background,
                 borderRadius: BorderRadius.only(
                   bottomLeft: Radius.circular(50.0),
                   bottomRight: Radius.circular(50.0),
@@ -91,7 +91,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
               right: 100.0,
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  primary: Theme.of(context).colorScheme.secondary,
+                  backgroundColor: Theme.of(context).colorScheme.secondary,
                   shape: StadiumBorder(),
                   padding: EdgeInsets.symmetric(vertical: 15),
                 ),
