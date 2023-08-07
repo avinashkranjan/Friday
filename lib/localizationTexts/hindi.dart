fa07a90f1f19fa0e66aae517e4822b851c9ce491
jaivsh <aarshorai22@gmail.com>
2023-08-07 17:03:55 +0530

    added localization support for multiple languages

---
+++ lib/localizationTexts/hindi.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+class HindiTexts {
+
+  String get friday => 'शुक्रवार';
+
+  
+  String get hellosir => 'नमस्ते महोदय!';
+
+  
+  String get recentalerts => 'हालिया अलर्ट';
+
+  
+  String get viewall => 'सभी देखें';
+
+  
+  String get seefavourites => 'पसंदीदा देखें';
+
+  
+  String get search => 'खोजो';
+
+  
+  String get recenthomework => 'हालिया होमवर्क';
+
+  
+  String get favourites => 'पसंदीदा';
+
+  
+  String get clearallfavourites => 'सभी पसंदीदा साफ़ करें';
+
+  
+  String get hoursleft => 'घंटे बाकी है';
+
+  
+  String get addnewclass => 'नई कक्षा जोड़ें';
+
+  
+  String get mode => 'प्रकार';
+
+  
+  String get teachername => 'शिक्षक का नाम';
+
+  
+  String get date => 'तारीख';
+
+  
+  String get time => 'समय';
+
+  
+  String get joinlink => 'लिंक से जुड़ें';
+
+  
+  String get subject => 'विषय';
+
+  
+  String get save => 'इसे जोड़ें';
+
+  
+  String get yourprofile => 'आपकी प्रोफ़ाइल';
+
+  
+  String get loading => 'लोड हो रहा है...';
+
+  
+  String get email => 'ईमेल';
+
+  
+  String get bio => 'बायो';
+
+  
+  String get college => 'कॉलेज';
+
+  
+  String get course => 'पाठ्यक्रम प्रकार';
+
+  
+  String get departmentmajor => 'विभाग';
+
+  
+  String get currentacademicyear => 'वर्तमान शैक्षणिक वर्ष';
+
+  
+  String get gender => 'लिंग';
+
+  
+  String get age => 'आयु';
+
+  
+  String get settings => 'सेटिंग';
+
+  
+  String get common => 'सामान्य';
+
+  
+  String get language => 'भाषा';
+
+  
+  String get lightmode => 'प्रकाश मोड';
+
+  
+  String get notifications => 'सूचनाएं';
+
+  
+  String get notificationsdesc => 'ऐप के नोटिफिकेशन को चालू या बंद करें';
+
+  
+  String get help => 'मदद';
+
+  
+  String get helpdesc => 'प्रश्नों के लिए हमारे सहायता मॉड्यूल देखें';
+
+  
+  String get faqs => 'सामान्य प्रश्न';
+
+  
+  String get faqsdesc => 'बुनियादी उपयोग दिशानिर्देश';
+
+  
+  String get supportdevelopment => 'ऐप के विकास का समर्थन करें';
+
+  
+  String get supportdevelopmentdesc => 'समस्याओं का समाधान करें, बग की रिपोर्ट करें, इत्यादि|';
+}
