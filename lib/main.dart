52eccd6d54edc9378c942f41c270a5adc36a6cd8
jaivsh <aarshorai22@gmail.com>
2023-08-09 11:42:16 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -132,7 +132,7 @@ class _MyAppState extends State<MyApp> {
             GlobalWidgetsLocalizations.delegate,
             GlobalCupertinoLocalizations.delegate,
           ],
-          locale: Locale('ar'),
+          locale: Locale('en'),
           supportedLocales: [
             Locale('ru'),
             Locale('en'), // English
